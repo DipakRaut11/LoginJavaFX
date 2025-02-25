b26e08b152a2549eb027710396f701a06a797d4a
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-02-25 09:04:15 +0545

    build database connection using JDBC

--- src/main/java/org/example/loginpage/dbConnection/DbCOnnection.java
+++ src/main/java/org/example/loginpage/dbConnection/DbCOnnection.java
@@ -1,4 +1,16 @@
 package org.example.loginpage.dbConnection;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbCOnnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/avocado_login";
+    private static final String USER = "mysql";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
