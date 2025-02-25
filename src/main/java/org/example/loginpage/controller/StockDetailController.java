70690a75b550c46ce1d6c3952bf5888102c57347
Dipak Raut <dipakkumarraut1119@gmail.com>
2025-02-25 09:03:32 +0545

    build controller for Stock detail

--- src/main/java/org/example/loginpage/controller/StockDetailController.java
+++ src/main/java/org/example/loginpage/controller/StockDetailController.java
@@ -1,2 +1,36 @@
-package org.example.loginpage.controller;public class StockDetailController {
+package org.example.loginpage.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class StockDetailController {
+
+
+        @FXML
+        private Label stockSymbolLabel;
+        @FXML
+        private Label stockPriceLabel;
+        @FXML
+        private Label stockFullNameLabel;
+        @FXML
+        private Label stockMarketLabel;
+        @FXML
+        private Label stockNameLabel;
+
+        public void setStockDetails(DashboardController.Stock stock) {
+            stockSymbolLabel.setText("Symbol: " + stock.getSymbol());
+            stockPriceLabel.setText("Price: $" + stock.getPrice());
+            stockFullNameLabel.setText("Full Name: " + stock.getFullName());
+            stockMarketLabel.setText("Market: " + stock.getMarket());
+            stockNameLabel.setText(stock.getFullName());
+        }
+
+        @FXML
+        private void handleClose() {
+            Stage stage = (Stage) stockSymbolLabel.getScene().getWindow();
+            stage.close();
+        }
+
+
 }
